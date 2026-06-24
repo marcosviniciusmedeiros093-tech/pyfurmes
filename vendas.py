777715f69d6d618af769260a75275c6d5c2f9789
Marcos Vinicius <marcos.vinicius.medeiros.093@ufrn.edu.br>
2026-06-24 18:56:23 -0300

    vendas
    dicionário utilizado no código principal

--- vendas.py
+++ vendas.py
@@ -1,4 +1,4 @@
-001,[clientes['123'][0],produtos['001'][0],produtos['001'][1],10.00,True,
-002,[clientes['345'][0],produtos['002'][0],produtos['002'][1],10.00,True,
-003,[clientes['456'][0],produtos['003'][0],produtos['003'][1],10.00,True,
-004,[clientes['678'][0],produtos['004'][0],produtos['004'][1],10.00,True,
+001,clientes['123'][0],produtos['001'][0],produtos['001'][1],10.00,True,
+002,clientes['345'][0],produtos['002'][0],produtos['002'][1],10.00,True,
+003,clientes['456'][0],produtos['003'][0],produtos['003'][1],10.00,True,
+004,clientes['678'][0],produtos['004'][0],produtos['004'][1],10.00,True,
