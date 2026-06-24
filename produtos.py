e9ff28aa6980f50830e7124339b0ff7b157e02a8
Marcos Vinicius <marcos.vinicius.medeiros.093@ufrn.edu.br>
2026-06-24 18:49:11 -0300

    produtos

--- produtos.py
+++ produtos.py
@@ -1,7 +1,5 @@
-produtos={
-    '001':['Ivone','perfume a',15.00,20.00,10],
-    '002':['Ivone','perfume b',15.00,20.00,10],
-    '003':['Ivone','perfume c',15.00,20.00,10],
-    '004':['Ivone','perfume d',15.00,20.00,10],
-    '005':['Ivone','perfume e',15.00,20.00,10],
-}
+001,Ivone,perfume a,15.00,20.00,10,True
+002,Ivone,perfume b,15.00,20.00,10,True
+003,Ivone,perfume c,15.00,20.00,10,True
+004,Ivone,perfume d,15.00,20.00,10,True
+005,Ivone,perfume e,15.00,20.00,10,True
