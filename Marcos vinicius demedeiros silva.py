79f8ac750f32bf2d3fd96f560dcf328164490c5c
Marcos Vinicius <marcos.vinicius.medeiros.093@ufrn.edu.br>
2026-06-24 19:01:37 -0300

    pyfurmes Versão Semi-final
    falta pouco

--- Marcos vinicius demedeiros silva.py
+++ Marcos vinicius demedeiros silva.py
@@ -23,11 +23,11 @@ try:
     arq_clientes.close()
 except:
     clientes={
-        '123' : ["Homer Simpson",'0933863826', "homer@springfield.com",'rua', "99999-9999",True],
-        '234' : ["Marge Simpson",'0933863826', "marge@springfield.com",'rua', "88888-8888",True],
-        '345' : ["Bart Simpson",'0933863826', "bart@springfield.com",'rua', "77777-7777",True],
-        '456' : ["Lisa Simpson",'0933863826', "lisa@springfield.com",'rua', "66666-6666",True],
-        '678' : ["Maggie Simpson",'0933863826', "maggie@springfield.com",'rua', "55555-5555",True]
+        '123' : ["Homer Simpson",'0933863826', "homer@springfield.com",'rua', "99999-9999",'True'],
+        '234' : ["Marge Simpson",'0933863826', "marge@springfield.com",'rua', "88888-8888",'True'],
+        '345' : ["Bart Simpson",'0933863826', "bart@springfield.com",'rua', "77777-7777",'True'],
+        '456' : ["Lisa Simpson",'0933863826', "lisa@springfield.com",'rua', "66666-6666",'True'],
+        '567' : ["Maggie Simpson",'0933863826', "maggie@springfield.com",'rua', "55555-5555",'True']
     }
     arq_clientes = open("clientes.py", "wt", encoding="utf-8")
     for posc, dados in clientes.items():
@@ -35,7 +35,64 @@ except:
     arq_clientes.close()
 
 produtos={}
+try:
+    arq_produtos = open("produtos.py", "rt", encoding="utf-8")
+    for linha in arq_produtos:
+        linha = linha.strip()
+        if linha:
+            campos = linha.split(",")
+            posp= campos[0]
+            marca= campos[1]
+            nomep= campos[2]
+            preco= campos[3]
+            valor= campos[4]
+            estoque= campos[5]
+            cad= campos[6]
+            produtos[posp]=[marca,nomep,preco,valor,estoque,cad,]
+    arq_produtos.close()
+except:
+    produtos={
+        '001' : [",Ivone",'Perfume a', "15.00",'20.00', "10",'True'],
+        '002' : [",Ivone",'Perfume b', "15.00",'20.00', "10",'True'],
+        '003' : [",Ivone",'Perfume c', "15.00",'20.00', "10",'True'],
+        '004' : [",Ivone",'Perfume d', "15.00",'20.00', "10",'True'],
+        '005' : [",Ivone",'Perfume e', "15.00",'20.00', "10",'True']
+    }
+    arq_produtos = open("produtos.py", "wt", encoding="utf-8")
+    for posp, dados in produtos.items():
+        arq_produtos.write(f"{posp},{dados[0]},{dados[1]},{dados[2]},{dados[3]},{dados[4]},{dados[5]}\n")
+    arq_produtos.close()
+
 vendas={}
+try:
+    arq_vendas = open("vendas.py", "rt", encoding="utf-8")
+    for linha in arq_vendas:
+        linha = linha.strip()
+        if linha:
+            campos = linha.split(",")
+            posv= campos[0]
+            nomev= campos[1]
+            marcav= campos[2]
+            nomepv= campos[3]
+            valorv= campos[4]
+            cad= campos[5]
+            lnomev=nomev.strip('',"")
+            lmarcav=marcav.strip('',"")
+            lnomepv=nomepv.strip('',"")
+            vendas[posv]=[lnomev,lmarcav,lnomepv,valorv,cad,]
+    arq_vendas.close()
+except:
+    vendas={
+        '001' : [clientes['123'][0],produtos['001'][0],produtos['001'][1],10.00,'True'],
+        '002' : [clientes['234'][0],produtos['002'][0],produtos['002'][1],10.00,'True'],
+        '003' : [clientes['345'][0],produtos['003'][0],produtos['003'][1],10.00,'True'],
+        '004' : [clientes['567'][0],produtos['004'][0],produtos['004'][1],10.00,'True'],
+}
+    arq_vendas = open("vendas.py", "wt", encoding="utf-8")
+    for posv, dados in vendas.items():
+        arq_vendas.write(f"{posv},{dados[0]},{dados[1]},{dados[2]},{dados[3]},{dados[4]}\n")
+    arq_vendas.close()
+
 while seletor!=0:
     seletor=int(input('''
     para sair digite 0
@@ -62,13 +119,13 @@ while seletor!=0:
             tel=input('digite o telefone para contato: ')
             mail=input('digite o E-mail')
             endereco=input('digite o endereço')
-            clientes[posc]=[nome,cpf,mail,endereco,tel,True]
+            clientes[posc]=[nome,cpf,mail,endereco,tel,'True']
             print('clientes: ',clientes)
             print('cadastro concluido')
 
         elif seletorc==2:
             posc=input('digite a numeração do cliente cadastado: ')
-            if posc in clientes and clientes[posc][5]==True:
+            if posc in clientes and clientes[posc][5]=='True':
                 print(' Dados atuais do cliente:')
                 print(' Nome     :', clientes[posc][0])
                 print(' CPF    :', clientes[posc][1])
@@ -81,24 +138,24 @@ while seletor!=0:
                 tel=input('digite o telefone para contato: ')
                 mail=input('digite o E-mail')
                 endereco=input('digite o endereço')
-                clientes[posc]=[nome,cpf,mail,endereco,tel,True]
+                clientes[posc]=[nome,cpf,mail,endereco,tel,'True']
                 print('alteração concluida')
             else:
                 print('cliente não encontrado')
 
         elif seletorc==3:
             posc=input('digite a numeração do cliente que você quer deletar: ')
-            if posc in clientes and clientes[posc][5]==True:
+            if posc in clientes and clientes[posc][5]=='True':
                 salvador=input(' você tem certeza? S/N ')
                 if salvador=='s' or salvador=='S':
-                    clientes[posc][5]=False
+                    clientes[posc][5]='False'
                     print('cliente removido com sucesso')
                 else:
                     print('exclusão cancelada')
 
         elif seletorc==4:
             posc=input('digite a numeração do cliente que você quer visualizar: ')
-            if posc in clientes and clientes[posc][5]==True:
+            if posc in clientes and clientes[posc][5]=='True':
                 print(' Dados atuais do cliente:')
                 print(' Nome     :', clientes[posc][0])
                 print(' CPF    :', clientes[posc][1])
@@ -125,11 +182,11 @@ while seletor!=0:
             preco=float(input('escreva o preço de mercado do produto: '))
             valor=float(input('escreva o valor do produto para seus clientes: '))
             estoque=int(input('digite o quantidade desse produto no o estoque: '))
-            produtos[posp]=[marca,nomep,preco,valor,estoque,True]
+            produtos[posp]=[marca,nomep,preco,valor,estoque,'True']
 
         elif seletorp==2:
             posp=int(input('digite a numeração do produto cadastado: '))
-            if posp in produtos and produtos[posp][5]==True:
+            if posp in produtos and produtos[posp][5]=='True':
                 print(' Dados atuais do produto:')
                 print(' Marca     :', produtos[posp][0])
                 print(' Nome    :', produtos[posp][1])
@@ -139,20 +196,20 @@ while seletor!=0:
                 print()
                 marca=input('escreva o nome da marca: ')
                 nomep=input('escreva o nome do produto: ')
-                preco=float(input('escreva o preço de mercado do produto: '))
-                valor=float(input('escreva o valor do produto para seus clientes: '))
+                preco=float(input('escreva o preço de mercado do produto: (utilize .) '))
+                valor=float(input('escreva o valor do produto para seus clientes : (utilize .) '))
                 estoque=int(input('digite o quantidade desse produto no o estoque: '))
-                produtos[posp]=[marca,nomep,preco,valor,estoque,True]
+                produtos[posp]=[marca,nomep,preco,valor,estoque,'True']
                 print('alteração concluida')
             else:
                 print('produto não encontrado')
 
         elif seletorp==3:
             posp=(input('digite a numeração do produto que você quer deletar: '))
-            if posp in produtos and produtos[posp][5]==True:
+            if posp in produtos and produtos[posp][5]=='True':
                 salvador=input(' você tem certeza? S/N ')
                 if salvador=='s' or salvador=='S':
-                    produtos[posp][5]=False
+                    produtos[posp][5]='False'
                     print('produto removido com sucesso')
                 else:
                     print('exclusão cancelada')
@@ -161,7 +218,7 @@ while seletor!=0:
 
         elif seletorp==4:
             posp=input('digite a numeração do produto que você quer visualizar: ')
-            if posp in produtos and produtos[posp][5]==True:
+            if posp in produtos and produtos[posp][5]=='True':
                 print(' Dados atuais do produto:')
                 print(' Marca     :', produtos[posp][0])
                 print(' Nome    :', produtos[posp][1])
@@ -191,34 +248,34 @@ while seletor!=0:
             posc=input('digite a numerção do cliente que comprou')
             if posp in produtos and posc in clientes:
                 valorv=input('digite o valor da venda')
-                vendas[posv]=[clientes[posc][0],produtos[posp][0],produtos[posp][1],valorv,True]
+                vendas[posv]=[clientes[posc][0],produtos[posp][0],produtos[posp][1],valorv,'True']
 
         elif seletorv==2:
                 posv=input('digite a numerção da venda que você que alterar: ')
-                if posv in vendas and vendas[posv][4]==True:
+                if posv in vendas and vendas[posv][4]=='True':
                     print(' Dados atuais da venda:')
                     print(' Nome do cliente     :', vendas[posv][0])
                     print(' Marca do produto    :', vendas[posv][1])
                     print('Produto  :', vendas[posv][2])
                     print('Valor da venda : ',vendas[posv][3])
                     print()
-                    for posp in produtos and produtos[posp][5]==True:
+                    for posp in produtos and produtos[posp][5]=='True':
                         print(f'Nome do produto: {produtos[posp][1]}')
                     posp=input('digite a numerção do produto')
-                    for posc in clientes and clientes[posc][5]==True:
+                    for posc in clientes and clientes[posc][5]=='True':
                         print(f'Nome do cliente: {clientes[posc][0]}')
                     posc=input('digite a numerção do cliente que comprou')
-                    if posp in produtos and posc in clientes and produtos[posp][5]==True and clientes[posc][5]==True:
+                    if posp in produtos and posc in clientes and produtos[posp][5]=='True' and clientes[posc][5]=='True':
                         valorv=input('digite o valor da venda')
-                        vendas[posv]=[clientes[posc][0],produtos[posp][0],produtos[posp][1],valorv,True]
+                        vendas[posv]=[clientes[posc][0],produtos[posp][0],produtos[posp][1],valorv,'True']
                         print('venda alterada com sucesso')
 
         elif seletorv==3:
                 posv=(input('digite a numeração da venda que você quer deletar: '))
-                if posv in vendas and vendas[posv][4]==True:
+                if posv in vendas and vendas[posv][4]=='True':
                     salvador=input(' você tem certeza? S/N ')
                     if salvador=='s' or salvador=='S':
-                        vendas[posv][4]=False
+                        vendas[posv][4]='False'
                         print('venda removida com sucesso')
                     else:
                         print('exclusão cancelada')
@@ -227,7 +284,7 @@ while seletor!=0:
 
         elif seletorv==4:
                 posv=input('digite a numerção da venda que você que alterar: ')
-                if posv in vendas and vendas[posv][4]==True:
+                if posv in vendas and vendas[posv][4]=='True':
                     print(' Dados atuais da venda:')
                     print(' Nome do cliente     :', vendas[posv][0])
                     print(' Marca do produto    :', vendas[posv][1])
@@ -242,7 +299,7 @@ while seletor!=0:
                     posc=input('digite a numerção do cliente que comprou')
                     if posp in produtos and posc in clientes:
                         valorv=input('digite o valor da venda')
-                        vendas[posv]=[clientes[posc][0],produtos[posp][0],produtos[posp][1],valorv,True]
+                        vendas[posv]=[clientes[posc][0],produtos[posp][0],produtos[posp][1],valorv,'True']
                 msg=input('esta parte está em desenvolvimento digite ENTER para sair ')
 
     elif seletor==4:
@@ -258,9 +315,9 @@ while seletor!=0:
         digite 2 para mais informações dos clientes
         '''))
             for posc in clientes:
-                if clientes[posc][5]==True:
+                if clientes[posc][5]=='True':
                     print(f'''nome:{clientes[posc][0]} | Posição: {posc} | Status: cadastrado''')
-                elif clientes[posc][5]==False:
+                elif clientes[posc][5]=='False':
                     print(f'''nome:{clientes[posc][0]} | Posição: {posc} | Status: excluido''')
 
             if opcoes==1:
@@ -269,10 +326,10 @@ while seletor!=0:
                     recad=input('gostaria de re-cadastrar algum cliente excluido? S/N')
                     if recad=='S' or recad=='s':
                         posc=input('digite a posição do cliente:')
-                        if posc in clientes and clientes[posc][5]==True:
+                        if posc in clientes and clientes[posc][5]=='True':
                             print('o cliente já é cadastrado, escolha um que foi excluido')
-                        if posc in clientes and clientes[posc][5]==False:
-                            clientes[posc][5]=True
+                        if posc in clientes and clientes[posc][5]=='False':
+                            clientes[posc][5]='True'
                             print('cliente re-cadastrado com sucesso')
                     elif recad != 'n' and recad!='N':
                         print(' opção invalida')
@@ -300,9 +357,9 @@ while seletor!=0:
         digite 2 para mais informações de produtos
         '''))
             for posp in produtos:
-                if produtos[posp][5]==True:
+                if produtos[posp][5]=='True':
                     print(f'''Nome:{produtos[posp][1]} | Posição: {posp} | Status: cadastrado''')
-                elif produtos[posp][5]==False:
+                elif produtos[posp][5]=='False':
                     print(f'''Nome:{produtos[posp][1]} | Posição: {posp} | Status: excluido''')
 
             if opcoes==1:
@@ -311,10 +368,10 @@ while seletor!=0:
                     recad=input('gostaria de re-cadastrar algum produto excluido? S/N ')
                     if recad=='S' or recad=='s':
                         posp=input('digite a posição do produto:')
-                        if posp in produtos and produtos[posp][5]==True:
+                        if posp in produtos and produtos[posp][5]=='True':
                             print('o produto já é cadastrado, escolha um que foi excluido')
-                        elif posp in produtos and produtos[posp][5]==False:
-                            produtos[posp][5]=True
+                        elif posp in produtos and produtos[posp][5]=='False':
+                            produtos[posp][5]='True'
                             print('produto re-cadastrado com sucesso')
                     elif recad!='n' and recad!='n':
                         print(' opção invalida')
@@ -340,9 +397,9 @@ while seletor!=0:
         digite 2 para mais informações de vendas
         '''))
             for posv in vendas:
-                if vendas[posv][4]==True:
+                if vendas[posv][4]=='True':
                     print(f'''Nome do cliente: {vendas[posv][0]} | Nome do produto: {vendas[posv][2]} | Posição: {posv} | Valor da venda: {vendas[posv][3]} | Status: cadastrada''')
-                elif vendas[posv][4]==False:
+                elif vendas[posv][4]=='False':
                     print(f'''Nome do cliente: {vendas[posv][0]} | Nome do produto: {vendas[posv][2]} | Posição: {posv} | Valor da venda: {vendas[posv][3]} | Status: excluida''')
             if opcoes==1:
                 recad=''
@@ -350,10 +407,10 @@ while seletor!=0:
                     recad=input('gostaria de re-cadastrar alguma venda excluida? S/N ')
                     if recad=='S' or recad=='s':
                         posv=input('digite a posição da venda:')
-                        if posv in vendas and vendas[posv][4]==True:
+                        if posv in vendas and vendas[posv][4]=='True':
                             print('a venda já é cadastrada, escolha uma que foi excluida')
-                        if posv in vendas and vendas[posv][4]==False:
-                            vendas[posv][4]=True
+                        if posv in vendas and vendas[posv][4]=='False':
+                            vendas[posv][4]='True'
                             print('venda re-cadastrada com sucesso')
                     elif recad != 'n' and recad!='N':
                         print(' opção invalida')
@@ -385,7 +442,18 @@ while seletor!=0:
     else:
         erro=input('comando invalido digite ENTER para voltar a seleção: ')
 print('fim do programa')
+
 arq_clientes= open("clientes.py", "wt", encoding="utf-8")
 for posc, dados in clientes.items():
     arq_clientes.write(f"{posc},{dados[0]},{dados[1]},{dados[2]},{dados[3]},{dados[4]},{dados[5]}\n")
 arq_clientes.close()
+
+arq_produtos= open("produtos.py", "wt", encoding="utf-8")
+for posp, dados in produtos.items():
+    arq_produtos.write(f"{posp},{dados[0]},{dados[1]},{dados[2]},{dados[3]},{dados[4]},{dados[5]}\n")
+arq_produtos.close()
+
+arq_vendas= open("vendas.py", "wt", encoding="utf-8")
+for posv, dados in vendas.items():
+    arq_vendas.write(f"{posv},{dados[0]},{dados[1]},{dados[2]},{dados[3]},{dados[4]}\n")
+arq_vendas.close()
