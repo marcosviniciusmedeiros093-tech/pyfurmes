6c39e027f702e6a462f850a9f44d7a4911db4cfc
Marcos Vinicius <marcosviniciusdemedeirossilva@gmail.com>
2026-06-27 20:29:07 -0300

    Add files via upload

---
+++ validações.py
@@ -0,0 +1,20 @@
+def vcpf(cpf):    
+    if len(cpf) == 11 and cpf.isdigit():
+        return True
+def vmail(mail):
+    mail = mail.strip()
+    if mail.count('@') != 1:
+        return False
+    usuario, dominio = mail.split('@')
+    if not usuario or not dominio:
+        return False
+    if '.' not in dominio or dominio.startswith('.') or dominio.endswith('.'):
+        return False
+    if ' ' in mail:
+        return False
+    else:
+        return True
+def vtel(tel):
+    digitos=tel.strip('',"")
+    if len(digitos) < 10 or len(digitos) > 11 and not digitos.isdigit():
+        return False
