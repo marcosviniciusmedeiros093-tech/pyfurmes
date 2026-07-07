3908f5b7850c1eda26d118b15632516bb5454dc7
Marcos Vinicius <marcos.vinicius.medeiros.093@ufrn.edu.br>
2026-07-06 22:33:15 -0300

    resolvendo o espaçamento nos telefones
    Updated phone numbers for all clients in clientes.py.

--- clientes.py
+++ clientes.py
@@ -1,5 +1,5 @@
-123,Homer Simpson,0933863826,homer@springfield.com,rua, 99999-9999,True
-234,Marge Simpson,0933863826,marge@springfield.com,rua, 88888-8888,True
-345,Bart Simpson,0933863826,bart@springfield.com,rua, 77777-7777,True
-456,Lisa Simpson,0933863826,lisa@springfield.com,rua, 66666-6666,True
-567,Maggie Simpson,0933863826,maggie@springfield.com,rua, 55555-5555,True
+123,Homer Simpson,09338638261,homer@springfield.com,rua,99999999911,True
+234,Marge Simpson,09338638261,marge@springfield.com,rua,88888888811,True
+345,Bart Simpson,09338638261,bart@springfield.com,rua,77777777711,True
+456,Lisa Simpson,09338638261,lisa@springfield.com,rua,66666666611,True
+567,Maggie Simpson,09338638261,maggie@springfield.com,rua,55555555511,True
