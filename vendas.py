bf0cd07c8c118291865d7d11fef255752814a782
Marcos Vinicius <marcos.vinicius.medeiros.093@ufrn.edu.br>
2026-07-06 22:59:50 -0300

    vendas versão final
    Updated price format from 10.0 to 10.00 for consistency.

--- vendas.py
+++ vendas.py
@@ -1,4 +1,4 @@
-001,Homer Simpson,Ivone,perfume a,10.0,True
-002,Marge Simpson,Ivone,perfume b,10.0,True
-003,Bart Simpson,Ivone,perfume c,10.0,True
-004,Maggie Simpson,Ivone,perfume d,10.0,True
+001,Homer Simpson,Ivone,perfume a,10.00,True
+002,Marge Simpson,Ivone,perfume b,10.00,True
+003,Bart Simpson,Ivone,perfume c,10.00,True
+004,Maggie Simpson,Ivone,perfume d,10.00,True
